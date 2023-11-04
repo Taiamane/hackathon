a3584a54665258b63b0d298a584938ca39b4a14e
Taiamane <fujiura-taishu749@g.ecc.u-tokyo.ac.jp>
2023-11-04 22:07:20 +0900

    firebase追加、認証した後の処理は後で

--- my-app/src/firebase.ts
+++ my-app/src/firebase.ts
@@ -1,22 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 import { getAuth } from "firebase/auth";
 
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import firebase from 'firebase/compat/app';
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAkX8hao3qKDMLqPxsX1nTB-49_edbm3i8",
-  authDomain: "term4-taishu-fujiura.firebaseapp.com",
-  projectId: "term4-taishu-fujiura",
-  storageBucket: "term4-taishu-fujiura.appspot.com",
-  messagingSenderId: "169011336171",
-  appId: "1:169011336171:web:7d453bd64071adec927fae"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGEING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
