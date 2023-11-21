269e7fd679179850bd22f739f9a4e6755f1fb6d9
Taiamane <fujiura-taishu749@g.ecc.u-tokyo.ac.jp>
2023-11-21 20:57:16 +0900

    items消してみる

--- my-app/src/Addition.tsx
+++ my-app/src/Addition.tsx
@@ -26,7 +26,7 @@ const DirectInputForm: React.FC = () => {
     // フォームの内容を使用して必要なアクションを実行
     // バックエンドにリクエストを送信
   try { 
-    const response = await fetch('https://hackathon-back-zmir5vzwua-uc.a.run.app/items',
+    const response = await fetch('https://hackathon-back-zmir5vzwua-uc.a.run.app/',
      {
       method: 'POST', // POSTリクエストを送信（HTTPメソッドを適切に設定）
       headers: {
